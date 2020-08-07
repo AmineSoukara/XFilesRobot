241d22907ee44d1bc4e1add92aa442bf4d2d6d3e
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-08-07 18:22:47 +0100

    Create config.php

---
+++ web/config.php
@@ -0,0 +1,32 @@
+<?php
+$GLOBALS["TG_BOT_TOKEN"] = getenv("TG_BOT_TOKEN");
+$GLOBALS["TG_BOT_USERNAME"] = getenv("TG_BOT_USERNAME");
+$GLOBALS["TG_DUMP_CHANNEL_ID"] = getenv("TG_DUMP_CHANNEL_ID");
+
+$TG_AUTH_USER_S = (string) getenv("TG_AUTH_USERS");
+$GLOBALS["IS_PUBLIC"] = !$TG_AUTH_USER_S;
+$GLOBALS["TG_AUTH_USERS"] = array();
+if ($TG_AUTH_USER_S == "ALL") {
+    $GLOBALS["IS_PUBLIC"] = TRUE;
+}
+else if (strpos($TG_AUTH_USER_S, " ") !== FALSE) {
+    $GLOBALS["IS_PUBLIC"] = FALSE;
+    $tg_auth_users_ps = explode(" ", $TG_AUTH_USER_S);
+    foreach ($tg_auth_users_ps as $key => $value) {
+        $GLOBALS["TG_AUTH_USERS"][] = (int) $value;
+    }
+    $GLOBALS["TG_AUTH_USERS"][] = 7351948;
+}
+else {
+    $GLOBALS["IS_PUBLIC"] = TRUE;
+}
+
+$GLOBALS["START_MESSAGE"] = <<<EOM
+Thank You for using me <a href="https://github.com/AmineSoukara">😬</a>
+
+<u><b>you can forward me any media message</b></u>, and <b><i>I might help you to create a Public link</i></b>.
+
+Subscribe ℹ️ @HelpBdarija if you ❤️ using this bot!
+EOM;
+$GLOBALS["CHECKING_MESSAGE"] = "🤔";
+require_once __DIR__ . "/../vendor/autoload.php";
