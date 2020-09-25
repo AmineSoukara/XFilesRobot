17c2bd80ea5018c37ebe77285ce45a6964b9cd52
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-09-25 22:11:21 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -7,6 +7,7 @@ $bot_id = $GLOBALS["TG_BOT_TOKEN"];
 $bot = new PHPBot($bot_id);
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
+$ADMIN = 853393439;
 
 if (isset($update["message"])) {
     $message_id = $update["message"]["message_id"];
@@ -98,4 +99,82 @@ https://t.me/" . $GLOBALS["TG_BOT_USERNAME"] . "?start=" . "File" . "_" . $req_m
         "text" => $required_url,
         "disable_web_page_preview" => True
     ));
+}
+elseif($text == "/panel" && $chat_id == $ADMIN){
+sendaction($chat_id, typing);
+        bot('sendmessage', [
+                'chat_id' =>$chat_id,
+                'text' =>"اوامر المطور الخاصة بك ",
+                'parse_mode'=>'html',
+      'reply_markup'=>json_encode([
+            'keyboard'=>[
+              [
+              ['text'=>"الاحصائيات💭"],['text'=>"اذاعة رسالة💭"],['text'=>"اذاعة توجيه💭"]
+              ]
+              ],'resize_keyboard'=>true
+        ])
+            ]);
+        }
+elseif($text == "الاحصائيات💭" && $chat_id == $ADMIN){
+	sendaction($chat_id,'typing');
+    $user = file_get_contents("Member.txt");
+    $member_id = explode("\n",$user);
+    $member_count = count($member_id) -1;
+	sendmessage($chat_id , "عدد المستخدمين📒 : $member_count" , "html");
+}
+elseif($text == "اذاعة رسالة💭" && $chat_id == $ADMIN){
+    file_put_contents("data/$from_id/ali.txt","send");
+	sendaction($chat_id,'typing');
+	bot('sendmessage',[
+    'chat_id'=>$chat_id,
+    'text'=>"حسنا ارسل رسالتك الان 📒",
+    'parse_mode'=>'html',
+    'reply_markup'=>json_encode([
+      'keyboard'=>[
+	  [['text'=>'/panel']],
+      ],'resize_keyboard'=>true])
+  ]);
+}
+elseif($ali == "send" && $chat_id == $ADMIN){
+    file_put_contents("data/$from_id/ali.txt","no");
+	SendAction($chat_id,'typing');
+	bot('sendmessage',[
+    'chat_id'=>$chat_id,
+    'text'=>"تم الارسال",
+  ]);
+	$all_member = fopen( "Member.txt", "r");
+		while( !feof( $all_member)) {
+ 			$user = fgets( $all_member);
+			SendMessage($user,$text,"html");
+		}
+}
+elseif($text == "اذاعة توجيه💭" && $chat_id == $ADMIN){
+    file_put_contents("data/$from_id/ali.txt","fwd");
+	sendaction($chat_id,'typing');
+	bot('sendmessage',[
+    'chat_id'=>$chat_id,
+    'text'=>"ارسل للتوجيه",
+    'parse_mode'=>'html',
+    'reply_markup'=>json_encode([
+      'keyboard'=>[
+	  [['text'=>'/panel']],
+      ],'resize_keyboard'=>true])
+  ]);
+}
+elseif($ali == "fwd" && $chat_id == $ADMIN){
+    file_put_contents("data/$from_id/ali.txt","no");
+	SendAction($chat_id,'typing');
+	bot('sendmessage',[
+    'chat_id'=>$chat_id,
+    'text'=>"تم التوجيه",
+  ]);
+$forp = fopen( "Member.txt", 'r'); 
+while( !feof( $forp)) { 
+$fakar = fgets( $forp); 
+Forward($fakar, $chat_id,$message_id); 
+  } 
+   bot('sendMessage',[ 
+   'chat_id'=>$chat_id, 
+   'text'=>"تم بنجاح✅", 
+   ]);
 }
